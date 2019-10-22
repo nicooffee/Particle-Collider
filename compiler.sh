56c14c3c76aa430238bdd05df6ac128e11e782c3
Nicolás Honorato <39715885+nicooffee@users.noreply.github.com>
2019-10-22 17:07:01 -0300

    Martes 22/10

--- compiler.sh
+++ compiler.sh
@@ -1,2 +1,2 @@
-gcc main.c libraries/participant.c libraries/participant_list.c -lncurses -pthread &&\
-./a.out 100 10 -1 16666;
+gcc main.c libraries/participant.c libraries/participant_list.c -Wall -lncurses -pthread &&\
+./a.out 1000 5 -1 10000;
